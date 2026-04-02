c913aaead0a9a2a19473d22a7bcf5140a072caf6
CoderOMaster <keshavarorasci@gmail.com>
2026-04-03 02:34:39 +0530

    hf issues

--- apps/next-js/next.config.ts
+++ apps/next-js/next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), "@huggingface/transformers"];
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
