e66318ddf2fe5707e191272f8491fa3bfec0542c
CoderOMaster <keshavarorasci@gmail.com>
2026-04-03 09:47:11 +0530

    hf issues

--- apps/next-js/next.config.ts
+++ apps/next-js/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  serverExternalPackages: ["@inferedge/moss", "@huggingface/transformers"],
 };
 
 export default nextConfig;
